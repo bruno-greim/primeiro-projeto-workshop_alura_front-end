f3d117b8c052003f52a13723d93afa7a4d46c588
Bruno Greim <brunogreim@Brunos-MacBook-Pro.local>
2024-09-15 20:55:44 -0300

    Primeiro commit do projeto

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, conhecida como \"Fadinha\", é uma skatista brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou o mundo do skate.",
+        link: "https://es.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro. Sua técnica e determinação a colocaram como uma das principais promessas do judô brasileiro.",
+        link: "https://es.wikipedia.org/wiki/Beatriz_Souza", 
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+        link: "https://es.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
